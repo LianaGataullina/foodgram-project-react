78094f32dc79ad6f2f9ad2d637a3b8bd3f2a4d60
Лиана Гатауллина <Gataullina.lg@yandex.ru>
2022-08-25 16:47:42 +0300

    filters updated

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -6,7 +6,8 @@ from recipes.models import Ingredient, Recipe, Tag
 class RecipeFilter(filters.FilterSet):
     tags = filters.ModelMultipleChoiceFilter(
         queryset=Tag.objects.all(),
-        field_name='tag__slug'
+        field_name='tags__slug',
+        to_field_name = 'slug'
     )
     is_favorited = filters.BooleanFilter(method='get_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
